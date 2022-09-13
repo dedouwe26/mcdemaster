52a062a64e4c5bb137686358848d7eb280f7de19
dedouwe26 <63008025+dedouwe26@users.noreply.github.com>
2022-09-13 17:08:55 +0200

    weekly commit

--- main.py
+++ main.py
@@ -6,21 +6,19 @@ This python program will automaticly convert
 java minecraft code to a maven project and 
 it also can make a patcher program later on.
 '''
-from importlib.resources import path
+import requests
 import json
 import os
-import requests
 from pathlib import Path
 
-CLIENT='c'
-SERVER='s'
+CLIENT='client'
+SERVER='server'
 
 def download(url,path):
     with open(path,'w') as file:
         file.write(requests.get(url).content.decode('utf-8'))
 
 def createVersionManifest(manifest):
-    
     download(manifest,Path('tmp/version_manifest_v2.json'))
     with open(Path('tmp/version_manifest_v2.json'),'r') as file:
         latestData = json.load(file)
@@ -38,6 +36,19 @@ def getVersionManifest(version):
         print('This is not a valid version.')
         exit(1)  
 
+def getSideData(side, version):
+    if not side==CLIENT or not side==SERVER:
+        print('Invalid version side...')
+        exit(1)
+    with open(Path(f'tmp/{version}.json'),'r') as file:
+        downloadsData=json.load(file)['downloads']
+    if not Path('data/{version}').is_dir():
+        os.makedirs(Path('data/{version}'))
+    download(downloadsData[side]['url'],Path('data/{version}/{side}.jar'))
+    download(downloadsData[side+'_mappings']['url'],Path('data/{version}/{side}.txt'))
+    # TODO: return path
+    return 
+
 def main():
     manifestV2='https://piston-meta.mojang.com/mc/game/version_manifest_v2.json'
 
@@ -48,16 +59,8 @@ def main():
     version=input(f'The latest version is {release} and the latest snapshot is {snapshot}.\nPlease put your version in: ')
     versionManifestUrl=getVersionManifest(version)['url']
     download(versionManifestUrl,Path(f'tmp/{version}.json'))
-    side = input('Client or server side (c/s): ')
-    if side==CLIENT:
-        pass
-        # client
-    elif side==SERVER:
-        pass
-        # server
-    else: 
-        print('Invalid version side...')
-        exit(1)
+    side = input('Client or server side (client/server): ')
+    sideJarData, sideMappingsData = getSideData(side,version)
         
     
 
